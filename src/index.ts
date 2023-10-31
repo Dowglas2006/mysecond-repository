cffb819bf790096c813162aad365423c201b09a9
Dowglas2006 <147665430+Dowglas2006@users.noreply.github.com>
2023-10-31 00:32:20 +0000

    fgcvgf

--- src/index.ts
+++ src/index.ts
@@ -9,17 +9,46 @@
 
 // cachorro.chamar()
 
-class usuario { 
-    senha: number 
-    nome: string
-    saldo: number
-    constructor( senhaFornecido: number, nomeFornecido: string, saldoFornecido: number) {
-        this.senha = senhaFornecido
-        this.nome = nomeFornecido
-        this.saldo = saldoFornecido
-    }    
+// class usuario { 
+//     senha: number 
+//     nome: string
+//     saldo: number
+//     constructor( senhaFornecido: number, nomeFornecido: string, saldoFornecido: number) {
+//         this.senha = senhaFornecido
+//         this.nome = nomeFornecido
+//         this.saldo = saldoFornecido
+//     }    
+// }
+
+// const usuario1 = new usuario (1322, "Dowglas", 0 )
+
+// console.log(usuario1)
+
+import { BankAccount } from "./classes/BankAccount";
+import {Transaction} from "./classes/Trasaction"
+
+const ba1 = new BankAccount("Dowglas", "0232302dg", "0232302");
+const ba2 = new BankAccount("Valdenia", "0222201vl", "0222201")
+const tr1 = new Transaction("0232302dg", "02222vl", 159000)
+
+function realizaTransacao (
+    idRemetente: string,
+    idDestinatario:string, 
+    valor:number
+     ) {
+    return 
 }
 
-const usuario1 = new usuario (1322, "Dowglas", 0 )
+function deposito (
+    ba1
+    valor
+    ) {
+    return ba1Alterada
+}
 
-console.log(usuario1)
+
+function saque (ba, valor){
+    return baAlterada
+}
+
+console.log(tr1)
